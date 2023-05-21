813baa40d2f5cd8c69fa843bcccef8dedfad992e
MichaelP <gpark608@gmail.com>
2023-05-20 18:32:46 -0600

    test

--- kakao_friends.py
+++ kakao_friends.py
@@ -5,24 +5,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 KAKAO_TOKEN = os.getenv('KAKAO_TOKEN')
-friend_url = "https://kapi.kakao.com/v1/api/talk/friends"
+def getFriendsList():
+    header = {"Authorization": 'Bearer ' + KAKAO_TOKEN}
+    url = "https://kapi.kakao.com/v1/api/talk/friends" #친구 정보 요청
 
-# GET /v1/api/talk/friends HTTP/1.1
-# Host: kapi.kakao.com
-# Authorization: Bearer {ACCESS_TOKEN}
+    result = json.loads(requests.get(url, headers=header).text)
 
-headers={"Authorization" : f"Bearer {KAKAO_TOKEN}"}
+    friends_list = result.get("elements")
+    friends_id = []
 
-result = json.loads(requests.get(friend_url, headers=headers).text)
+    print(requests.get(url, headers=header).text)
+    print(friends_list)
 
-print(type(result))
-print("=============================================")
-print(result)
-print("=============================================")
-friends_list = result.get("elements")
-print(friends_list)
-# print(type(friends_list))
-print("=============================================")
-print(friends_list[0].get("uuid"))
-friend_id = friends_list[0].get("uuid")
-print(friend_id)
+    for friend in friends_list:
+        friends_id.append(str(friend.get("uuid")))
+
+        return friends_id
+
+getFriendsList()
